3db818f5c2e25ac218c65b9ca099315191d09ccb
bszczerba13 <bartekk29alk@gmail.com>
2026-03-15 11:21:37 +0100

    Coose gender

--- pages/create_account_page.py
+++ pages/create_account_page.py
@@ -1,14 +1,19 @@
 from selenium.webdriver.common.by import By
-
 from pages.base_page import BasePage
 from time import sleep
+from utils.custom_types import Gender
 
 class Locators:
     FIRST_NAME = (By.ID, "customer_firstname")
+    GENDER_MALE = (By.XPATH, '//label[@for="id_gender1"]')
+    GENDER_FEMALE = (By.XPATH, '//label[@for="id_gender2"]')
 
 class CreateAccountPage(BasePage):
     def choose_gender(self, gender):
-        pass
+        if gender == Gender.MALE:
+            self.driver.find_element(*Locators.GENDER_MALE).click()
+        else:
+            self.driver.find_element(*Locators.GENDER_FEMALE).click()
     def enter_firstname(self, firstname):
         self.driver.find_element(*Locators.FIRST_NAME).send_keys(firstname)
 
--- tests/registration_test.py
+++ tests/registration_test.py
@@ -1,13 +1,16 @@
 from tests.base_test import BaseTest
 from time import sleep
+from test_data.registration_data_generator import RegistrationDataGenerator
 
 class RegistrationTest(BaseTest):
     def setUp(self):
         super().setUp()
+        self.data = RegistrationDataGenerator()
         self.authentication_page = self.home_page.click_sign_in()
-        self.authentication_page.enter_create_account_email("erferf@erf.com")
+        self.authentication_page.enter_create_account_email(self.data.EMAIL)
         self.create_account_page = self.authentication_page.click_create_account()
 
     def testNoLastname(self):
-        self.create_account_page.enter_firstname("Marcin")
+        self.create_account_page.choose_gender(self.data.GENDER)
+        self.create_account_page.enter_firstname(self.data.FIRST_NAME)
         sleep(3)
